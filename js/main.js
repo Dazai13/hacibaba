d281a26067551a2dcd6259e5908e3ed446e3e09e
Dazai13 <dazai1317@mail.ru>
2025-06-26 19:03:45 +0700

    Готовое интро

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.intro__slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:true,
+  autoplay:5000,
+  prevArrow:'.intro__arrow-prev',
+  nextArrow:'.intro__arrow-next',
+});
